955e97c7d79ae929110ef3481cde6795b21fc3ab
fankesyooni <qzmmcn@163.com>
2023-12-19 13:50:40 +0800

    feat: add DisplayDensity in DimensionFactory

--- ui-extension/src/main/java/com/highcapable/betterandroid/ui/extension/component/base/DimensionFactory.kt
+++ ui-extension/src/main/java/com/highcapable/betterandroid/ui/extension/component/base/DimensionFactory.kt
@@ -24,9 +24,112 @@
 
 package com.highcapable.betterandroid.ui.extension.component.base
 
+import android.app.Dialog
 import android.content.Context
 import android.content.res.Resources
 import android.view.View
+import android.view.Window
+import androidx.fragment.app.Fragment
+
+/**
+ * Display density interface.
+ *
+ * Wherever you need to use px and dp, just implement it as an interface on the current class.
+ *
+ * Supports: [Context], [Window], [View], [Resources], [Fragment], [Dialog].
+ *
+ * Usage:
+ *
+ * ```kotlin
+ * class MainActivity : AppCompatActivity(), DisplayDensity {
+ *
+ *    override fun onCreate(savedInstanceState: Bundle?) {
+ *        super.onCreate(savedInstanceState)
+ *        setContentView(R.layout.activity_main)
+ *        // Convert dp to px.
+ *        val tenDp = 10.dp
+ *        // Or you can use it like this.
+ *        val tenDp = dp.toPx()
+ *        // Convert px to dp.
+ *        val thirtySixPx = 36.px
+ *        // Or you can use it like this.
+ *        val thirtySixPx = px.toDp()
+ *    }
+ * }
+ * ```
+ */
+interface DisplayDensity {
+
+    /**
+     * Convert dp to px.
+     * @see toPx
+     * @receiver the original dimension.
+     * @return [N]
+     */
+    val <N : Number> N.dp: N get() = toPx()
+
+    /**
+     * Convert px to dp.
+     * @see toDp
+     * @receiver the original dimension.
+     * @return [N]
+     */
+    val <N : Number> N.px: N get() = toDp()
+
+    /**
+     * Convert dp to px.
+     *
+     * Looks like: 10dp → 36px (Please refer to the target device).
+     * @see dp
+     * @receiver the original dimension.
+     * @return [N]
+     */
+    fun <N : Number> N.toPx() = resolvePx(dimension = this)
+
+    /**
+     * Convert px to dp.
+     *
+     * Looks like: 36px → 10dp (Please refer to the target device).
+     * @see px
+     * @receiver the original dimension.
+     * @return [N]
+     */
+    fun <N : Number> N.toDp() = resolveDp(dimension = this)
+}
+
+/**
+ * Resolve the px dimension from [DisplayDensity].
+ * @receiver [DisplayDensity]
+ * @param dimension the original dimension.
+ * @return [N]
+ * @throws IllegalStateException if the current [DisplayDensity] is not supported.
+ */
+private fun <N : Number> DisplayDensity.resolvePx(dimension: N) = when (this) {
+    is Context -> dimension.toPx(context = this)
+    is Window -> dimension.toPx(context)
+    is View -> dimension.toPx(context)
+    is Resources -> dimension.toPx(resources = this)
+    is Fragment -> dimension.toPx(resources)
+    is Dialog -> dimension.toPx(context)
+    else -> error("DisplayDensity only supports Context, Window, View, Resources, Fragment and Dialog.")
+}
+
+/**
+ * Resolve the dp dimension from [DisplayDensity].
+ * @receiver [DisplayDensity]
+ * @param dimension the original dimension.
+ * @return [N]
+ * @throws IllegalStateException if the current [DisplayDensity] is not supported.
+ */
+private fun <N : Number> DisplayDensity.resolveDp(dimension: N) = when (this) {
+    is Context -> dimension.toDp(context = this)
+    is Window -> dimension.toDp(context)
+    is View -> dimension.toDp(context)
+    is Resources -> dimension.toDp(resources = this)
+    is Fragment -> dimension.toDp(resources)
+    is Dialog -> dimension.toDp(context)
+    else -> error("DisplayDensity only supports Context, Window, View, Resources, Fragment and Dialog.")
+}
 
 /**
  * Convert dp to px.
