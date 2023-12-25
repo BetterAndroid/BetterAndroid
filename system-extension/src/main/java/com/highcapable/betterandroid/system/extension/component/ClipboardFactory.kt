98c663ff7139c99b12ce9b3c597595349637c95b
fankesyooni <qzmmcn@163.com>
2023-12-25 23:39:26 +0800

    refactor: add ClipDataItemBuilder and new way to use clipboard in ClipboardFactory

--- system-extension/src/main/java/com/highcapable/betterandroid/system/extension/component/ClipboardFactory.kt
+++ system-extension/src/main/java/com/highcapable/betterandroid/system/extension/component/ClipboardFactory.kt
@@ -28,50 +28,179 @@ import android.app.Dialog
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.View
 import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
+import com.highcapable.yukireflection.factory.current
+
+/**
+ * Clip data item's builder.
+ */
+class ClipDataItemBuilder internal constructor() {
+
+    /** The items of clip data. */
+    private val dataItems = mutableListOf<ClipData.Item>()
+
+    /**
+     * Add a text item to clip data.
+     * @param text the text to add.
+     */
+    fun addText(text: CharSequence) {
+        dataItems.add(ClipData.Item(text))
+    }
+
+    /**
+     * Add a html text item to clip data.
+     * @param text the text to add.
+     * @param htmlText the html text to add.
+     */
+    fun addHtmlText(text: CharSequence, htmlText: String) {
+        dataItems.add(ClipData.Item(text, htmlText))
+    }
+
+    /**
+     * Add a intent item to clip data.
+     * @param intent the intent to add.
+     */
+    fun addIntent(intent: Intent) {
+        dataItems.add(ClipData.Item(intent))
+    }
+
+    /**
+     * Add a uri item to clip data.
+     * @param uri the uri to add.
+     */
+    fun addUri(uri: Uri) {
+        dataItems.add(ClipData.Item(uri))
+    }
+
+    /**
+     * Build clip data items.
+     * @return [MutableList]<[ClipData.Item]>
+     */
+    internal fun build() = dataItems
+}
+
+/**
+ * Create a clip data.
+ * @param mimeTypes the mime types of clip data.
+ * @param label the clip data visible label, default is null.
+ * @param initiate the [ClipDataItemBuilder] builder body.
+ * @throws IllegalStateException if no clip data item provided.
+ */
+@JvmOverloads
+fun ClipData(
+    vararg mimeTypes: String,
+    label: CharSequence? = null,
+    initiate: ClipDataItemBuilder.() -> Unit
+): ClipData {
+    val dataItems = ClipDataItemBuilder().apply(initiate).build()
+    require(dataItems.isNotEmpty()) { "ClipData must have at least one item." }
+    return ClipData(label, mimeTypes, dataItems[0]).apply { dataItems.drop(1).forEach { addItem(it) } }
+}
 
 /**
  * Copy text to clipboard.
+ * @receiver [ClipboardManager]
+ * @param text the text to copy.
+ * @param label the clip data visible label, default is null.
+ */
+@JvmOverloads
+fun ClipboardManager.copy(text: CharSequence, label: CharSequence? = null) =
+    copy(ClipData.newPlainText(label, text))
+
+/**
+ * Copy html text to clipboard.
+ * @receiver [ClipboardManager]
+ * @param text the text to copy.
+ * @param htmlText the html text to copy.
+ * @param label the clip data visible label, default is null.
+ */
+@JvmOverloads
+fun ClipboardManager.copy(text: CharSequence, htmlText: String, label: CharSequence? = null) =
+    copy(ClipData.newHtmlText(label, text, htmlText))
+
+/**
+ * Copy intent to clipboard.
+ * @receiver [ClipboardManager]
+ * @param intent the intent to copy.
+ * @param label the clip data visible label, default is null.
+ */
+@JvmOverloads
+fun ClipboardManager.copy(intent: Intent, label: CharSequence? = null) =
+    copy(ClipData.newIntent(label, intent))
+
+/**
+ * Copy uri to clipboard.
+ * @receiver [ClipboardManager]
+ * @param uri the uri to copy.
+ * @param label the clip data visible label, default is null.
+ */
+@JvmOverloads
+fun ClipboardManager.copy(uri: Uri, label: CharSequence? = null) {
+    val resolverFromManager = current(ignored = true)
+        .field { name = "mContext" }
+        .cast<Context>()?.contentResolver
+    copy(ClipData.newUri(resolverFromManager, label, uri))
+}
+
+/**
+ * Copy to clipboard.
+ * @receiver [ClipboardManager]
+ * @param clipData the clip data to copy.
+ */
+fun ClipboardManager.copy(clipData: ClipData) = setPrimaryClip(clipData)
+
+/**
+ * Get clipboard manager.
  * @receiver the current context.
- * @param text the text to copy.
- * @param label the clip data visible label, default is null.
- * @return [Boolean] whether succeed.
+ * @return [ClipboardManager]
+ * @throws IllegalStateException if clipboard manager is unavailable.
  */
-@JvmOverloads
-fun Context.copyToClipboard(text: CharSequence, label: CharSequence? = null) =
-    getSystemService<ClipboardManager>()?.let { manager ->
-        manager.setPrimaryClip(ClipData.newPlainText(label, text))
-        (manager.primaryClip?.getItemAt(0)?.text ?: "") == text
-    } ?: false
+val Context.clipboardManager get() = getSystemService<ClipboardManager>() ?: error("ClipboardManager is unavailable.")
 
 /**
  * Copy text to clipboard.
- * @receiver the current fragment.
- * @param text the text to copy.
- * @param label the clip data visible label, default is null.
- * @return [Boolean] whether succeed.
+ *
+ * - This function is deprecated, use [Context.clipboardManager] instead.
+ * @see Context.clipboardManager
+ * @see ClipboardManager.copy
  */
+@Deprecated(message = "Use Context.clipboardManager instead.", ReplaceWith("clipboardManager.copy(text, label)"))
 @JvmOverloads
-fun Fragment.copyToClipboard(text: CharSequence, label: CharSequence? = null) = requireContext().copyToClipboard(text, label)
+fun Context.copyToClipboard(text: CharSequence, label: CharSequence? = null) = clipboardManager.copy(text, label)
 
 /**
  * Copy text to clipboard.
- * @receiver the current view.
- * @param text the text to copy.
- * @param label the clip data visible label, default is null.
- * @return [Boolean] whether succeed.
+ *
+ * - This function is deprecated, use [Context.clipboardManager] instead.
+ * @see Context.clipboardManager
+ * @see ClipboardManager.copy
  */
+@Deprecated(message = "Use Context.clipboardManager instead.", ReplaceWith("context?.clipboardManager?.copy(text, label)"))
 @JvmOverloads
-fun View.copyToClipboard(text: CharSequence, label: CharSequence? = null) = context.copyToClipboard(text, label)
+fun Fragment.copyToClipboard(text: CharSequence, label: CharSequence? = null) = context?.clipboardManager?.copy(text, label)
 
 /**
  * Copy text to clipboard.
- * @receiver the current dialog.
- * @param text the text to copy.
- * @param label the clip data visible label, default is null.
- * @return [Boolean] whether succeed.
+ *
+ * - This function is deprecated, use [Context.clipboardManager] instead.
+ * @see Context.clipboardManager
+ * @see ClipboardManager.copy
  */
+@Deprecated(message = "Use Context.clipboardManager instead.", ReplaceWith("context?.clipboardManager?.copy(text, label)"))
 @JvmOverloads
-fun Dialog.copyToClipboard(text: CharSequence, label: CharSequence? = null) = context.copyToClipboard(text, label)
+fun View.copyToClipboard(text: CharSequence, label: CharSequence? = null) = context?.clipboardManager?.copy(text, label)
+
+/**
+ * Copy text to clipboard.
+ *
+ * - This function is deprecated, use [Context.clipboardManager] instead.
+ * @see Context.clipboardManager
+ * @see ClipboardManager.copy
+ */
+@Deprecated(message = "Use Context.clipboardManager instead.", ReplaceWith("context.clipboardManager.copy(text, label)"))
+@JvmOverloads
+fun Dialog.copyToClipboard(text: CharSequence, label: CharSequence? = null) = context.clipboardManager.copy(text, label)
