c2562fa58db5b558f049d10c4120ee2312dfb9a9
fankesyooni <qzmmcn@163.com>
2023-12-27 22:48:26 +0800

    feat: add startServiceOrElse in ServiceFactory

--- system-extension/src/main/java/com/highcapable/betterandroid/system/extension/component/ServiceFactory.kt
+++ system-extension/src/main/java/com/highcapable/betterandroid/system/extension/component/ServiceFactory.kt
@@ -73,3 +73,53 @@ inline fun Context.startService(packageName: String, serviceClass: String, initi
 @RequiresApi(SystemVersion.O)
 inline fun Context.startForegroundService(packageName: String, serviceClass: String, initiate: Intent.() -> Unit = {}) =
     startForegroundService(Intent().apply { component = ComponentName(packageName, serviceClass) }.apply(initiate))
+
+/**
+ * Start a [Service] instance [T].
+ * @receiver the current context.
+ * @param initiate the [Intent] builder body, default is empty.
+ * @return [Boolean] whether succeed.
+ */
+inline fun <reified T : Service> Context.startServiceOrElse(initiate: Intent.() -> Unit = {}) =
+    runCatching { startService<T>(initiate) }.isSuccess
+
+/**
+ * Start a foreground [Service] instance [T].
+ * @receiver the current context.
+ * @param initiate the [Intent] builder body, default is empty.
+ * @return [Boolean] whether succeed.
+ */
+@RequiresApi(SystemVersion.O)
+inline fun <reified T : Service> Context.startForegroundServiceOrElse(initiate: Intent.() -> Unit = {}) =
+    runCatching { startForegroundService<T>(initiate) }.isSuccess
+
+/**
+ * Start a [Service] using [ComponentName].
+ * @receiver the current context.
+ * @param packageName the target package name.
+ * @param serviceClass the target app [Service] class name.
+ * @param initiate the [Intent] builder body, default is empty.
+ * @return [Boolean] whether succeed.
+ */
+inline fun Context.startServiceOrElse(packageName: String, serviceClass: String, initiate: Intent.() -> Unit = {}) =
+    runCatching { startService(packageName, serviceClass, initiate) }.isSuccess
+
+/**
+ * Start a foreground [Service] using [ComponentName].
+ * @receiver the current context.
+ * @param packageName the target package name.
+ * @param serviceClass the target app [Service] class name.
+ * @param initiate the [Intent] builder body, default is empty.
+ * @return [Boolean] whether succeed.
+ */
+@RequiresApi(SystemVersion.O)
+inline fun Context.startForegroundServiceOrElse(packageName: String, serviceClass: String, initiate: Intent.() -> Unit = {}) =
+    runCatching { startForegroundService(packageName, serviceClass, initiate) }.isSuccess
+
+/**
+ * Start a [Service]
+ * @receiver the current context.
+ * @param intent the intent to start.
+ * @return [Boolean] whether succeed.
+ */
+fun Context.startServiceOrElse(intent: Intent) = runCatching { startService(intent) }.isSuccess
