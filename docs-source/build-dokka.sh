1ab49496d42d26d18daac0e33210c97daf6fabd6
fankesyooni <qzmmcn@163.com>
2024-01-02 02:00:15 +0800

    chore: update build-dokka

--- docs-source/build-dokka.sh
+++ docs-source/build-dokka.sh
@@ -1,4 +1,4 @@
 cd ..
-./gradlew :ui-component:publishKDoc :ui-extension:publishKDoc :system-extension:publishKDoc :compose-extension:publishKDoc
+./gradlew :ui-component:publishKDoc :ui-extension:publishKDoc :system-extension:publishKDoc :compose-extension:publishKDoc :compose-multiplatform:publishKDoc
 # TODO: When the permission-extension library is done.
 # :permission-extension:publishKDoc
