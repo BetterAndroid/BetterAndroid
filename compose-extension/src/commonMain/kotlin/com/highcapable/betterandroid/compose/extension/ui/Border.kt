d364ea5df1a57e84009bc02ad222eaf2bd27d41f
fankesyooni <qzmmcn@163.com>
2024-01-07 05:22:15 +0800

    refactor: split Border

--- compose-extension/src/commonMain/kotlin/com/highcapable/betterandroid/compose/extension/ui/Border.kt
+++ compose-extension/src/commonMain/kotlin/com/highcapable/betterandroid/compose/extension/ui/Border.kt
@@ -25,22 +25,49 @@ package com.highcapable.betterandroid.compose.extension.ui
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
-import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.isSpecified
 
 /**
- * Workaround for [BorderStroke.width] is 0.dp, which will cause the [Modifier.border] still draw a border.
+ * Workaround for [BorderStroke.width] is 0.dp like [Dp.Hairline], which will cause the [Modifier.border] still draw a border,
+ * see [here](https://stackoverflow.com/questions/72514987/unexpected-border-in-composables-border-shows-even-if-border-width-is-zero).
  *
- * This function will stop draw the border if the [BorderStroke.width] is 0.dp.
+ * This function will stop draw the border if the [width] is 0.dp,
+ * I tink the scheme of set the border color to transparent is stupid.
+ * @see border
  * @receiver [Modifier]
- * @param border the border stroke.
- * @param shape the shape of the border, default is [RectangleShape].
+ * @param width the border stroke width.
+ * @param brush the border stroke brush.
+ * @param shape the shape of the border stroke, default is [RectangleShape].
  * @return [Modifier]
  */
-@Stable
-fun Modifier.borderOrElse(border: BorderStroke, shape: Shape = RectangleShape) =
-    border.takeIf { it.width.isSpecified && it.width > 0.dp }?.let { border(it, shape) } ?: this
+fun Modifier.borderOrElse(width: Dp, brush: Brush, shape: Shape = RectangleShape) =
+    if (width.isSpecified && width > 0.dp) border(width, brush, shape) else this
+
+/**
+ * Workaround for [BorderStroke.width] is 0.dp like [Dp.Hairline], you can see another [borderOrElse] for more details.
+ * @see borderOrElse
+ * @receiver [Modifier]
+ * @param width the border stroke width.
+ * @param color the border stroke solid color.
+ * @param shape the shape of the border stroke, default is [RectangleShape].
+ * @return [Modifier]
+ */
+fun Modifier.borderOrElse(width: Dp, color: Color, shape: Shape = RectangleShape) = borderOrElse(width, SolidColor(color), shape)
+
+/**
+ * Workaround for [BorderStroke.width] is 0.dp like [Dp.Hairline], you can see another [borderOrElse] for more details.
+ * @see borderOrElse
+ * @receiver [Modifier]
+ * @param border the border stroke.
+ * @param shape the shape of the border stroke, default is [RectangleShape].
+ * @return [Modifier]
+ */
+fun Modifier.borderOrElse(border: BorderStroke, shape: Shape = RectangleShape) = borderOrElse(border.width, border.brush, shape)
